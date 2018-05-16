d7224d5b1eb167e30b4368ea472f017dee77c826
fayaazn <fnabie@roc-dev.com>
2018-05-16 11:20:15 +0200

    Javascript Fayaaz Nabie

---
+++ h2/aapmetjs/aapmetjs.js
@@ -0,0 +1,4 @@
+document.getElementById('paginatitel').innerText = 'Mijn geweldige pagina';
+document.getElementById('subkop').innerText = 'Mijn geweldige subkop';
+document.getElementById('aap1').src = 'img/157763cff77b0ed814f624bc72a9ff42.png';
+document.getElementById('aap2').src = 'img/1098a6d882897bb947d2daf3196ab030.png';
